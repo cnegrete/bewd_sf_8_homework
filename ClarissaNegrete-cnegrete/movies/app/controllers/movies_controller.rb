7d5a3bb84e1981204b10c312e2124593a30656cb
Clarissa Negrete <clarissa@clarissa-mbp.corp.dropbox.com>
2015-03-26 14:53:14 -0700

    finished movie app

--- ClarissaNegrete-cnegrete/movies/app/controllers/movies_controller.rb
+++ ClarissaNegrete-cnegrete/movies/app/controllers/movies_controller.rb
@@ -2,5 +2,61 @@ class MoviesController < ApplicationController
   def index
   	@movies = Movie.all
   end
+
+   def show
+   	 @movie = Movie.find(params[:id])
+   end
+
+   def new
+    @movie = Movie.new
+	end
+  
+   def edit
+   	@movie = Movie.find(params[:id])
+   end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to @movie, notice: 'Movie was successfully added.' }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @movie = Movie.find(params[:id])
+        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    respond_to do |format|
+      format.html { redirect_to animals_url, notice: 'Movie was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:title, :description, :year_updated)
+    end
 end
+
  
--- ClarissaNegrete-cnegrete/movies/config/routes.rb
+++ ClarissaNegrete-cnegrete/movies/config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
+  resources :movies
 
-  root 'movies#index'
+
+
   
-  # get 'movies/index'
-
-  # get 'movies/show'
-
-  # get 'movies/new'
-
-  resources 'movies', only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
