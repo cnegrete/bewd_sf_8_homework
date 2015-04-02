40024e7b7aeab1346ba375df7cfab87d167cf16a
Clarissa Negrete <clarissa.negrete@gmail.com>
2015-04-01 19:49:05 -0700

    adding rewsly

---
+++ ClarissaNegrete-cnegrete/Rewsly/app/controllers/stories_controller.rb
@@ -0,0 +1,29 @@
+class StoriesController < ApplicationController
+def index
+    @stories = params[:q] ? Story.search_for(params[:q]) : Story.all
+  end
+
+  def show
+    @story = Story.find params[:id]
+  end
+
+  def edit
+    @story = Story.find params[:id]
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    safe_story_params = params.require(:story).permit(:title, :link, :category)
+    @story = Story.new safe_story_params.merge(:upvotes => 1)
+    @story.user = current_user
+
+    if @story.save
+      redirect_to @story
+    else
+      render :new
+    end
+  end
+end
